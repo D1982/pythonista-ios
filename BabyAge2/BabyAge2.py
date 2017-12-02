c3baf3199ad20dd158aaa17a2176cf66ff2b8cf3
Daniel Ott <d>
2017-12-02 01:41:24 +0000

    Make the sources runnable again

--- BabyAge2/BabyAge2.py
+++ BabyAge2/BabyAge2.py
@@ -2,70 +2,75 @@ import ui
 import datetime
 from dateutil import rrule
 
+def between_dates(rule, start_date, end_date):
+		delta = rrule.rrule(rule, dtstart=start_date, until=end_date)
+		return delta.count()
 
-class BabyAge
-
-	DOB
-
-	weeks = null
-	months = null
-	years = null
-
-	def between_dates(rule, start_date, end_date):
-		weeks = rrule.rrule(rule, dtstart=start_date, until=end_date)
-		return weeks.count()
-
-	DOB = datetime.datetime.now()
+class BabyAge2:
 	NOW = datetime.datetime.now()
+	child1 = 'Smilla'
+	dob = None
+	vroot = None
+	
+	weeks = None
+	months = None
+	years = None
+	full_weeks = None
+	
+	def __init__(self, dob):
+		self.dob = dob
+		
+	def datepicker_action(self, sender):
+		self.dob = sender.date
+		self.calc()
+		self.set()
 
-	full_weeks = (NOW - DOB).days/7
-
-	weeks = between_dates(rrule.WEEKLY, DOB, NOW)
-	months = between_dates(rrule.MONTHLY, DOB, NOW)
-	years = between_dates(rrule.YEARLY, DOB, NOW)
-
-	#print 'Full weeks {}'.format(full_weeks)
-	#print '{}th week'.format(weeks)
-	#print 'Months {}'.format(months)
-
-	def button_smilla_action(sender):
+	def button_child1_action(self, sender):
+		self.dob = datetime.datetime(2017, 9, 8, 2, 44)
 		v = sender.superview
-		DOB = datetime.datetime(2017, 9, 8, 2, 44)
-		v['datepicker'].date = DOB
-		calc()
-		display()
+		v['datepicker'].date = self.dob
+		self.calc()
+		self.set()
 		
+	def calc(self):
+		self.weeks = between_dates(rrule.WEEKLY, self.dob, self.NOW)
+		self.months = between_dates(rrule.MONTHLY, self.dob, self.NOW)
+		self.years = between_dates(rrule.YEARLY, self.dob, self.NOW)
 		
-	def datepicker_action(sender):
-		dob = sender.date
-		calc()
+	def set(self):
+		self.vroot['weeks'].text = str(self.weeks) + '.'
+		self.vroot['months'].text = str(self.months) + '.'
+		self.vroot['years'].text = str(self.years) + '.'
 		
-	def calc():
-		weeks = between_dates(rrule.WEEKLY, dob, NOW)
-		months = between_dates(rrule.MONTHLY, dob, NOW)
-		years = between_dates(rrule.YEARLY, dob, NOW)
+	def display(self):
+		self.vroot = ui.load_view('BabyAge2')
 		
-	def display():
-		v = ui.load_view('BabyAge2')
+		# Bind actions to class methods
+		self.vroot['button_child1'].action = self.button_child1_action
 		
-		v['weeks'].text = str(weeks) + '.'
-		v['months'].text = str(months) + '.'
-		v['years'].text = str(years) + '.'
+		self.vroot['button_child1'].title = self.child1
+			
+		self.vroot['datepicker'].action = self.datepicker_action
 		
+		self.vroot['datepicker'].date = self.dob
+		
+		self.vroot['weeks'].enabled = False
+		self.vroot['months'].enabled = False
+		self.vroot['years'].enabled = False
+		
+		self.vroot.present('sheet')
 
-
-		v['datepicker'].date = DOB
-		v['weeks'].enabled = False
-		v['months'].enabled = False
-		v['years'].enabled = False
-		v['weeks'].text = str(weeks) + '.'
-		v['months'].text = str(months) + '.'
-		v['years'].text = str(years) + '.'
+# Avoid warnings due to dynamic binding
+def datepicker_action(sender):
+	pass
+	
+def button_child1_action(sender):
+	pass
 		
-		v.present('sheet')
-		
-	def main():
-		display()
+def main():
+	babyage = BabyAge2(datetime.datetime(1982, 10, 22, 7, 20))
+	babyage.calc()
+	babyage.display()
 
 if __name__ == '__main__':
   main()
